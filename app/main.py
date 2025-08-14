12c8687776f97f2fda7684f40edc30ea264bf485
SHIN-Bootcamp <shin99463040@gmail.com>
2025-08-14 15:14:28 +0900

    Add files via upload
    quiz router 추가 및 불러오기

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 # add router files
 from app.routers import login
 from app.routers import execute
+from app.routers import quiz  # <--- quiz router 추가
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ def read_root():
 
 app.include_router(login.router)            # 로그인 관련 기능
 app.include_router(execute.router)          # 데이터 수집 코드 파일 실행 기능
+app.include_router(quiz.router)             # quiz 관련 기능
