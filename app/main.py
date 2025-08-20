b569075a6915b6a0b81cbc65f9870a9060071d5b
main3373 <main3373@gmail.com>
2025-08-20 09:50:04 +0900

    update main.py

--- app/main.py
+++ app/main.py
@@ -34,10 +34,10 @@ def reset_day_process():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """ A function to run when the server starts """
-    scheduler.add_job(get_news_datas, CronTrigger(hour = 10, minute = 00))      # AM 10:00
+    scheduler.add_job(get_news_datas, CronTrigger(hour = 10, minute = 0))      # AM 10:00
     scheduler.add_job(get_news_datas, CronTrigger(hour = 15, minute = 30))      # PM 3:30
-    scheduler.add_job(get_news_datas, CronTrigger(hour = 18, minute = 00))      # PM 6:00
-    scheduler.add_job(reset_day_process, CronTrigger(hour = 0, minute = 00))    # AM 12:00
+    scheduler.add_job(get_news_datas, CronTrigger(hour = 18, minute = 0))      # PM 6:00
+    scheduler.add_job(reset_day_process, CronTrigger(hour = 0, minute = 0))    # AM 12:00
 
     scheduler.start()
 
